f393ca563fa657d1b5529379e6005d2e255917e0
Timothy Moliterno <tim@admithub.com>
2020-06-19 11:26:50 -0400

    comment, handle non-numeric multiplier

--- js/multiplyAndPreserve.js
+++ js/multiplyAndPreserve.js
@@ -1,5 +1,7 @@
 /**
- * Strips non-numbers from multiplier, then finds the product of that and any numbers in the input.
+ * Function written for Google Sheets. Accepts a single cell or a column of cells and outputs the same.
+ *
+ * Strips non-numbers from multiplier, then finds the product of that and any numbers in the input cell or column.
  * Strings in the input are ignored during operation and preserved. Rounds to two decimal places.
  *
  * @param {string|Array<string>|number|Array<number>} input - The value or range of cells to be multiplied
@@ -18,7 +20,11 @@ function multiplyAndPreserve(input, multiple) {
     typeof multiple === "number"
       ? multiple
       : parseFloat(multiple.replace(/[^\d.]/g, ""));
-  const inputList = !Array.isArray(input) ? [input] : input;
+  if (!sanitizedMultiple) {
+    return null;
+  }
+  const inputList = Array.isArray(input) ? input : [input];
+  //TODO: Handle 2d arrays
   return inputList.map((value) => {
     const segments = value.toString().match(/\D+|\d*\.?\d+/g);
     return segments === null
