d621a8c1c96125d3dceaaa8caa33e41575328750
Timothy Moliterno <tim@admithub.com>
2020-06-19 11:26:50 -0400

    comment, handle non-numeric multiplier, correct naming

--- js/multiplyAndPreserve.js
+++ js/multiplyAndPreserve.js
@@ -1,24 +1,30 @@
 /**
- * Strips non-numbers from multiplier, then finds the product of that and any numbers in the input.
+ * Function written for Google Sheets. Accepts a single cell or a column of cells and outputs the same.
+ *
+ * Strips non-numbers from multiplier, then finds the product of that and any numbers in the input cell or column.
  * Strings in the input are ignored during operation and preserved. Rounds to two decimal places.
  *
  * @param {string|Array<string>|number|Array<number>} input - The value or range of cells to be multiplied
- * @param {string|number} multiple
+ * @param {string|number} multiplier
  * @return - Any numbers in the input multiplied by the multiple, with strings preserved in between
  * @customfunction
  */
-function multiplyAndPreserve(input, multiple) {
-  if (!multiple) {
+function multiplyAndPreserve(input, multiplier) {
+  if (!multiplier) {
     return input;
   }
   if (!input) {
     return [];
   }
-  const sanitizedMultiple =
-    typeof multiple === "number"
-      ? multiple
-      : parseFloat(multiple.replace(/[^\d.]/g, ""));
-  const inputList = !Array.isArray(input) ? [input] : input;
+  const sanitizedMultiplier =
+    typeof multiplier === "number"
+      ? multiplier
+      : parseFloat(multiplier.replace(/[^\d.]/g, ""));
+  if (!sanitizedMultiplier) {
+    return null;
+  }
+  const inputList = Array.isArray(input) ? input : [input];
+  //TODO: Handle 2d arrays
   return inputList.map((value) => {
     const segments = value.toString().match(/\D+|\d*\.?\d+/g);
     return segments === null
@@ -27,7 +33,7 @@ function multiplyAndPreserve(input, multiple) {
           .map((segment) =>
             /^\d*\.?\d+$/.test(segment)
               ? (
-                  Math.round(parseFloat(segment) * sanitizedMultiple * 100) /
+                  Math.round(parseFloat(segment) * sanitizedMultiplier * 100) /
                   100
                 ).toString()
               : segment
